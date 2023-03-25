1b7b912c1b380258d353679498f495b88b0efb47
yogendrareddy <nallapa.yreddy@gmail.com>
2023-03-25 20:28:24 +0530

    modified es-lint errors

--- COE1_HW7/homework/app.js
+++ COE1_HW7/homework/app.js
@@ -12,3 +12,339 @@ externalService.getCountryListByLanguage()
 get countries list by region
 externalService.getCountryListByRegion()
 */
+/* eslint-disable no-magic-numbers */
+
+appRoot.classList.add('container');
+const form = document.createElement('form');
+const br = document.createElement('br');
+const h1 = document.createElement('h1');
+h1.innerText = 'Countries Search';
+appRoot.appendChild(h1);
+
+const label = document.createElement('label');
+label.innerText = 'Please choose type of serach:';
+
+appRoot.appendChild(form);
+const radioRegion = document.createElement('input');
+const radioLabel = document.createElement('label');
+radioLabel.innerHTML = 'By region<br/>';
+radioRegion.type = 'radio';
+radioRegion.name = 'search';
+radioRegion.id = 'region';
+radioRegion.value = 'region';
+
+const div=document.createElement('div');
+div.id='radios'
+form.appendChild(div);
+div.appendChild(label)
+div.appendChild(radioRegion);
+div.appendChild(radioLabel);
+
+const languageRegion = document.createElement('input');
+const languageLabel = document.createElement('label');
+languageLabel.innerText = 'By Language';
+languageRegion.setAttribute('name', 'search');
+languageRegion.setAttribute('type', 'radio');
+languageRegion.setAttribute('id', 'language');
+languageRegion.setAttribute('value', 'language');
+
+div.appendChild(languageRegion);
+div.appendChild(languageLabel);
+
+const searchQuery = document.createElement('label');
+searchQuery.innerHTML = ' <br/>Please Chose search query:';
+form.appendChild(searchQuery);
+const select = document.createElement('select');
+select.disabled = true;
+select.id = 'select';
+select.appendChild(document.createElement('option')).textContent =
+  'Select value';
+form.appendChild(select);
+const div1=document.createElement('div');
+div1.id='tableId';
+form.appendChild(div1);
+
+const regionsList = externalService.getRegionsList();
+const languagesList = externalService.getLanguagesList();
+const radioId1 = document.getElementById('region');
+const radioId2 = document.getElementById('language');
+
+function countryIncreasing(){
+
+          let table, i, x, y;
+          table = document.getElementById('table2');
+          let switching = true;
+          while (switching) {
+            switching = false;
+            let rows = table.rows;
+            let shouldSwitch;         
+            for (i = 1; i < rows.length - 1; i++) {
+              shouldSwitch = false;
+              x = rows[i].getElementsByTagName('TD')[0];
+              y = rows[i + 1].getElementsByTagName('TD')[0];
+              if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+                shouldSwitch = true;
+                break;
+              }
+            }
+            if (shouldSwitch) {
+              rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+              switching = true;
+            }
+          }
+
+}
+function countryDecreasing(){
+        let table, i, x, y;
+        table = document.getElementById('table2');
+        let switching = true;
+        while (switching) {
+          switching = false;
+          let rows = table.rows;
+          let shouldSwitch;       
+          for (i = 1; i < rows.length - 1; i++) {
+            shouldSwitch = false;   
+            x = rows[i].getElementsByTagName('TD')[0];
+            y = rows[i + 1].getElementsByTagName('TD')[0];
+
+            if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+
+              shouldSwitch = true;
+              break;
+            }
+          }
+          if (shouldSwitch) {
+            rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+            switching = true;
+          }
+        }
+
+} 
+function areaIncreasing(){
+ 
+          let table, i, x, y;
+          table = document.getElementById('table2');
+          let switching = true;
+
+          while (switching) {
+            switching = false;
+            let rows = table.rows;
+            let shouldSwitch;
+
+            for (i = 1; i < rows.length - 1; i++) {
+              shouldSwitch = false; 
+              x = rows[i].getElementsByTagName('TD')[4];
+              y = rows[i + 1].getElementsByTagName('TD')[4];
+              if (Number(x.innerHTML) > Number(y.innerHTML)) {
+                shouldSwitch = true;
+                break;
+              }
+            }
+            if (shouldSwitch) {   
+              rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+              switching = true;
+            }
+          }
+        
+}
+function areaDecreasing(){
+
+  let table, i, x, y;
+  table = document.getElementById('table2');
+  let switching = true;
+
+  while (switching) {
+    switching = false;
+    let rows = table.rows;
+    let shouldSwitch;
+    for (i = 1; i < rows.length - 1; i++) {
+      shouldSwitch = false;
+      x = rows[i].getElementsByTagName('TD')[4];
+      y = rows[i + 1].getElementsByTagName('TD')[4];
+
+      if (Number(x.innerHTML) < Number(y.innerHTML)) {
+        shouldSwitch = true;
+        break;
+      }
+    }
+    if (shouldSwitch) {
+      rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+      switching = true;
+    }
+  }
+
+}
+function tableUpdate(optionValue,radioValue){
+  const div11=document.getElementById('tableId');
+  div11.innerText='';
+  const table = document.createElement('table');
+  table.id = 'table2';
+  const tbody = document.createElement('tbody');
+  table.appendChild(tbody);
+  const tr = document.createElement('tr');
+  tbody.appendChild(tr);
+  const th = document.createElement('th');
+  th.innerHTML = 'Country name   <span class=" arrow" value="increasing">&#8593;</span>';
+  tr.appendChild(th);
+  const th2 = document.createElement('th');
+  th2.innerText = 'Capital';
+  tr.appendChild(th2);
+  const th3 = document.createElement('th');
+  th3.innerText = 'World Region';
+  tr.appendChild(th3);
+  const th4 = document.createElement('th');
+  th4.innerText = 'Languages';
+  th4.classList.add('languages');
+  tr.appendChild(th4);
+  const th5 = document.createElement('th');
+  th5.innerHTML ='Area <span class="upDownArrow arrowR" value="unsort">&#8597;</span>';
+  tr.appendChild(th5);
+  const th6 = document.createElement('th');
+  th6.innerHTML = 'Flag';
+  tr.appendChild(th6);
+  div1.appendChild(table);
+  const listByLanguage =
+    externalService.getCountryListByLanguage(optionValue);
+  const listByRegion=externalService.getCountryListByRegion(optionValue);
+  let lists;
+  if(radioValue==='region'){
+    lists=listByRegion;
+  }else if(radioValue==='language'){
+    lists=listByLanguage;
+  }
+  lists.forEach((countryDetail) => {
+    const tr1 = document.createElement('tr');
+    tr1.id='trow';
+    tbody.appendChild(tr1);
+    for (let i=0; i < 6; ++i) {
+
+      const td = document.createElement('td');
+      tr1.appendChild(td);
+      if (i === 0) {
+        td.innerHTML = countryDetail['name'];
+      } else if (i === 1) {
+        td.innerHTML = countryDetail['capital'];
+      } else if (i === 2) {
+        td.innerHTML = countryDetail['region'];
+      } else if (i === 3) {
+        const languages = countryDetail['languages'];
+        for (const lang in languages) {
+          if (true) {
+            const element = languages[lang];
+            td.innerHTML += `${element}, `;
+          }
+        }
+      } else if (i === 4) {
+        td.innerHTML = countryDetail['area'];
+      } else if (i === 5) {
+        const img = document.createElement('img');
+        const url =countryDetail['flagURL'];
+        img.src=`${url}`
+        td.appendChild(img);
+      }
+    }
+
+  })
+  countryIncreasing();
+
+  const countryInc = document.querySelector('.arrow');
+let sort=countryInc.getAttribute('value');
+const areaInc = document.querySelector('.arrowR');
+  let sortA=areaInc.getAttribute('value');
+  countryInc.addEventListener('click',function(){
+    areaInc.innerHTML='&#8597;';
+    areaInc.setAttribute('value','unsort');
+    if(sort==='unsort'){
+      countryIncreasing();
+      countryInc.innerHTML='&#8593';
+    countryInc.setAttribute('value','increasing');
+    sort='increasing';
+    }else if(sort==='increasing'){
+
+      countryDecreasing();
+      countryInc.innerHTML='&#8595;'
+      countryInc.setAttribute('value','decreasing');
+      sort='decreasing';
+   }else if(sort==='decreasing'){
+    countryIncreasing()
+    countryInc.innerHTML='&#8593;'
+    countryInc.setAttribute('value','increasing');
+    sort='increasing';
+   }
+  })
+
+  areaInc.addEventListener('click',function(){
+    countryInc.innerHTML='&#8597;';
+    countryInc.setAttribute('value','unsort');
+    if(sortA==='unsort'){
+      areaIncreasing();
+      areaInc.innerHTML='&#8593';
+    areaInc.setAttribute('value','increasing');
+    sortA='increasing';
+    }else if(sortA==='increasing'){
+      areaDecreasing();
+      areaInc.innerHTML='&#8595';
+      areaInc.setAttribute('value','decreasing');
+      sortA='decreasing';
+   }else if(sortA==='decreasing'){
+    areaIncreasing()
+    areaInc.innerHTML='&#8593';
+    areaInc.setAttribute('value','increasing');
+    sortA='increasing';
+   }
+  })
+ 
+}
+const p = document.createElement('p');
+p.id='para1'
+p.innerText = '';
+p.innerText = 'No items, please choose search query';
+const r=document.getElementById('radios');
+r.addEventListener('change',function(e){
+let radioValue=e.target.value;
+select.innerHTML = '';
+select.appendChild(document.createElement('option')).textContent ='Select value';
+  const selectElement = document.getElementById('select');
+if(radioValue==='region'){
+  regionsList.forEach((region) => {
+    const option = document.createElement('option');
+    option.value = region;
+    option.textContent = region;
+    select.appendChild(option);
+  });
+  if(selectElement.value==='Select value'){
+    const tableDiv = document.getElementById('tableId');
+      tableDiv.innerText = '';
+    p.innerText = 'No items, please choose search query';
+    form.appendChild(p)
+  }
+
+  selectElement.addEventListener('input', function (e) {
+    p.innerText='';
+    let optionValue = e.target.value;
+    // eslint-disable-next-line brace-style
+    tableUpdate(optionValue,radioValue) })
+}
+select.disabled = false;
+
+if(radioValue==='language'){
+  languagesList.forEach((language) => {
+    const option = document.createElement('option');
+    option.value = language;
+    option.textContent = language;
+    select.appendChild(option);
+  });
+  if(selectElement.value==='Select value'){
+    const tableDiv = document.getElementById('tableId');
+      tableDiv.innerText = '';
+    p.innerText = 'No items, please choose search query';
+    form.appendChild(p)
+  }
+  selectElement.addEventListener('input', function (e) {
+    p.innerText='';
+    let optionValue = e.target.value;
+    // eslint-disable-next-line brace-style
+    tableUpdate(optionValue,radioValue) })
+}
+select.disabled = false;
+})
--- COE1_HW8/homework/index.js
+++ COE1_HW8/homework/index.js
@@ -1,58 +1,117 @@
 /* START TASK 1: Your code goes here */
-// Get the table element
-const table = document.getElementById('myTable');
+const table = document.querySelector('table');
+const cells = document.querySelectorAll('td');
 
-// Get all cells in the table
-const cells = table.querySelectorAll('.cell');
+let selectedCells = [];
+let selectedRows = [];
 
-// Add event listeners to cells
-cells.forEach((cell) => {
-  cell.addEventListener('click', () => {
-    // Toggle yellow background color
-    if (cell.style.backgroundColor !== 'yellow') {
-      cell.style.backgroundColor = 'yellow';
-    } else {
-      cell.style.backgroundColor = 'white';
-    }
-
-    // Toggle blue background color on row
-    const row = cell.parentNode;
-    if (cell.cellIndex === 0) {
-      if (row.style.backgroundColor !== 'blue') {
-        row.style.backgroundColor = 'blue';
-      } else {
-        row.style.backgroundColor = '';
-      }
-    }
-
-    // Toggle yellow background color on whole table
-    if (cell.classList.contains('special')) {
-      cells.forEach((cell) => {
-        if (cell.classList.contains('special')) {
-          cell.style.backgroundColor = 'green';
-        } else {
-          cell.style.backgroundColor = 'yellow';
-        }
-      });
-    }
-
-    // Set priority of colors
-    cells.forEach((cell) => {
-      if (cell.style.backgroundColor === 'yellow') {
-        cell.style.backgroundColor = 'yellow';
-      } else if (cell.parentNode.style.backgroundColor === 'blue') {
-        cell.style.backgroundColor = 'blue';
-      } else {
-        cell.style.backgroundColor = 'white';
+function toggleSelectedCell(cell) {
+  if (cell.classList.contains('selected')) {
+    cell.classList.remove('selected');
+    cell.style.backgroundColor = '';
+    selectedCells.splice(selectedCells.indexOf(cell), 1);
+  } else {
+    cell.classList.add('selected');
+    cell.style.backgroundColor = 'yellow'; 
+    cell.style.backgroundColor = 'blue';  
+    selectedCells.push(cell);
+  }
+}
+function toggleSelectedRow(row) {
+  if (row.classList.contains('selected-row')) {
+    row.classList.remove('selected-row');
+    row.style.backgroundColor = '';
+    selectedRows.splice(selectedRows.indexOf(row), 1);
+  } else {
+    row.classList.add('selected-row');
+    row.style.backgroundColor = 'blue';
+    selectedRows.push(row);
+  }
+}
+function toggleTableColor() {
+  if (table.classList.contains('selected')) {
+    table.classList.remove('selected');
+    cells.forEach(cell => {
+      if (!cell.classList.contains('selected')) {
+        cell.style.backgroundColor = '';
       }
     });
+    selectedCells = [];
+    selectedRows.forEach(row => {
+      if (!row.classList.contains('selected-row')) {
+        row.style.backgroundColor = '';
+      }
+    });
+    selectedRows = [];
+  } else {
+    table.classList.add('selected');
+    cells.forEach(cell => {
+      if (!cell.classList.contains('selected')) {
+        cell.classList.add('selected');
+        cell.style.backgroundColor = 'yellow';
+        selectedCells.push(cell);
+      }
+    });
+  }
+}
+cells.forEach(cell => {
+  cell.addEventListener('click', () => {
+    if (cell.classList.contains('special')) {
+      toggleTableColor();
+    } else {
+      toggleSelectedCell(cell);
+      if (cell.classList.contains('col-1')) {
+        const row = cell.parentNode;
+        toggleSelectedRow(row);
+      }
+    }
   });
 });
-
+table.addEventListener('click', function(event) {
+  const clickedCell = event.target;
+  if (clickedCell.cellIndex === 0) {
+    const row = clickedCell.parentNode;
+    if (!row.classList.contains('selected-row')) {
+      toggleSelectedRow(row);
+    }
+  }
+});
 /* END TASK 1 */
 
 /* START TASK 2: Your code goes here */
-
+const phoneNumberInput = document.getElementById('phone-number');
+ const sendButton = document.getElementById('send-button');
+ const notification = document.getElementById('notification');
+ 
+ phoneNumberInput.addEventListener('input', validatePhoneNumber);
+ 
+ function validatePhoneNumber() {
+   const phoneNumber = phoneNumberInput.value.trim();
+   const regex = /^\+380\d{9}$/;
+ 
+   if (!regex.test(phoneNumber)) {
+     phoneNumberInput.classList.add('invalid');
+     sendButton.disabled = true;
+     notification.textContent = 'Type number does not follow format +380*********';
+     notification.classList.remove('hidden');
+     notification.classList.remove('success');
+     notification.classList.add('error');
+   } else {
+     phoneNumberInput.classList.remove('invalid');
+     sendButton.disabled = false;
+     notification.classList.add('hidden');
+     notification.classList.remove('error');
+   }
+ }
+ 
+ sendButton.addEventListener('click', showSuccessNotification);
+ function showSuccessNotification() {
+   const phoneNumber = phoneNumberInput.value.trim();
+   notification.textContent = `Data ${phoneNumber} was successfully sent.`;
+   notification.classList.remove('hidden');
+   notification.classList.remove('error');
+   notification.classList.add('success');
+ }
 /* END TASK 2 */
 
 /* START TASK 3: Your code goes here */
